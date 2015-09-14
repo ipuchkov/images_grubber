2a5c6928ed9b987e3bbf0cf76bfdd20483d10c0c
Ivan Puchkov <rake.eclipse@gmail.com>
2015-09-15 02:41:10 +0600

    little fix

--- spec/file_saver_spec.rb
+++ spec/file_saver_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe FileSaver do
   describe 'file' do
     context 'create file' do
       let(:data) { 'data' }
-      let(:name) { 'file.txt' }
+      let(:name) { '/tmp/test_file.txt' }
       let(:file) { FileSaver.new.write(name, data) }
 
       it do
+        file
+
         expect(File.exist?(name)).to be true
         File.delete(name)
       end
